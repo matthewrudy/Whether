8ea2d0598b57f681a0aa62c4481a6b1bd47c0ba4
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2011-02-16 00:46:16 +0800

    a first start

--- config.ru
+++ config.ru
@@ -1,7 +1,6 @@
 require "rubygems"
 require "bundler/setup"
 
-require 'sinatra'
 require 'whether'
 run Sinatra::Application
 
--- whether.rb
+++ whether.rb
@@ -1,3 +1,32 @@
+require 'open-uri'
+require 'sinatra'
+require 'nokogiri'
+
+def the_weather
+  noko = Nokogiri::XML(open("http://newsrss.bbc.co.uk/weather/forecast/85/ObservationsRSS.xml"))
+  text = noko.xpath('//item/title').text
+  text.split("\n")[1].split(".").first
+end
+
+helpers do
+
+  def weather_picture(string)
+    case string
+    when "grey cloud" then "cloudy5.png"
+    end
+  end
+
+  def weather_picture_or_string(string)
+    if picture = weather_picture(string)
+      %(<img src="/icons/#{picture}" />)
+    else
+      string.inspect
+    end
+  end
+  
+end
+
 get '/' do
-  "Hello World!"
+  @the_weather = the_weather
+  erubis :index
 end
