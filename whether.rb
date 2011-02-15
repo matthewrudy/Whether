6ac8d00e4e2cd286c35a61ea468ab45370c03f1d
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2011-02-16 00:59:29 +0800

    extra weathers and add an alt to the image

--- whether.rb
+++ whether.rb
@@ -12,13 +12,15 @@ helpers do
 
   def weather_picture(string)
     case string
-    when "grey cloud" then "cloudy5.png"
+    when "white cloud" then "cloudy5.png"
+    when "grey cloud" then "overcast.png"
+    when "light rain" then "light_rain.png"
     end
   end
 
   def weather_picture_or_string(string)
     if picture = weather_picture(string)
-      %(<img src="/icons/#{picture}" />)
+      %(<img src="/icons/#{picture}" alt=#{string.inspect} />)
     else
       string.inspect
     end
